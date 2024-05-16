8b7230ced13cc6856dc983b2e6ea06aa8a4be339
Anastasiya Shpakova <nastya.shpakova@list.ru>
2024-05-16 17:02:58 +0300

    'Corrections'

--- src/test/java/specs/MainSpec.java
+++ src/test/java/specs/MainSpec.java
@@ -8,6 +8,8 @@ import io.restassured.specification.ResponseSpecification;
 
 import static helpers.CustomAllureListener.withCustomTemplates;
 import static io.restassured.RestAssured.with;
+import static io.restassured.filter.log.LogDetail.BODY;
+import static io.restassured.filter.log.LogDetail.STATUS;
 
 public class MainSpec {
     public static RequestSpecification requestRegressInSpecification = with()
@@ -18,19 +20,25 @@ public class MainSpec {
             .log().uri();
 
     public static ResponseSpecification responseRegressInSpecificationStatusCode200 = new ResponseSpecBuilder()
-            .log(LogDetail.STATUS)
-            .log(LogDetail.BODY)
+            .log(STATUS)
+            .log(BODY)
             .expectStatusCode(200)
             .build();
     public static ResponseSpecification responseRegressInSpecificationStatusCode404 = new ResponseSpecBuilder()
-            .log(LogDetail.STATUS)
-            .log(LogDetail.BODY)
+            .log(STATUS)
+            .log(BODY)
             .expectStatusCode(404)
             .build();
 
     public static ResponseSpecification responseRegressInSpecificationStatusCode204 = new ResponseSpecBuilder()
-            .log(LogDetail.STATUS)
-            .log(LogDetail.BODY)
+            .log(STATUS)
+            .log(BODY)
             .expectStatusCode(204)
+            .build();
+
+    public static ResponseSpecification missingPasswordResponseSpec = new ResponseSpecBuilder()
+            .log(STATUS)
+            .log(BODY)
+            .expectStatusCode(400)
             .build();
 }
